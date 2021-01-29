5ac1948dc08cfb0f031da0f29150ead049e35a55
Elogeek <elodiechristin@gmail.com>
2021-01-29 11:57:50 +0100

    finish

--- result.php
+++ result.php
@@ -1,20 +1,24 @@
 <?php
 if (isset($_POST['go'], $_POST['number1'], $_POST['number2'])) {
-    $numberOne = strip_tags(trim($_POST['number1']));
-    $numberTwo = strip_tags(trim($_POST['number2']));
+    $numberOne = trim(strip_tags($_POST['number1']));
+    $numberTwo = trim(strip_tags($_POST['number2']));
     if ((intval($numberOne)) && (intval($numberTwo))) {
         if (($numberOne <= getrandmax()) || ($numberTwo <= getrandmax())) {
             if ($numberOne < $numberTwo) {
                 echo rand($numberOne, $numberTwo);
-            } else {
+            }
+            else {
                 echo rand($numberTwo, $numberOne);
             }
-        } else {
+        }
+        else {
             header('location: index.php?error = 3');
         }
-    } else {
+    }
+    else {
         header('location: index.php?error = 2');
     }
-} else {
+}
+else {
     header('location = index.php?error = 1');
 }
